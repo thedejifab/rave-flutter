a03678429a3aa8e20e8f76b1f91630a3d2a352e4
Fabusuyi012 <thedejifab@outlook.com>
2018-12-23 00:26:50 +0100

    Implementation of SubAccount class [3/4 way completed]

--- lib/sub_account.dart
+++ lib/sub_account.dart
@@ -1,13 +1,25 @@
-class SubAccount{
+class SubAccount {
+  static final String FLAT = "flat"; 
+  static final String PERCENTAGE = "percentage"; 
+
+  String _id; 
+  String _transactionSplitRatio; 
+  String _transactionChargeType; 
+  String _transactionCharge; 
+
+  String get getID => this._id; 
+  set setID(String id) => 
+      this._id = id; 
   
-    static final String FLAT = "flat";
-    static final String PERCENTAGE = "percentage";
+  String get getTransactionSplitRatio => this._transactionSplitRatio; 
+  set setTransactionSplitRatio(String transactionSplitRatio) => 
+      this._transactionSplitRatio = transactionSplitRatio; 
 
-    String _id;
-    String _transactionSplitRatio;
-    String _transactionChargeType;
-    String _transactionCharge;
+  String get getTransactionChargeType => this._transactionChargeType; 
+  set setTransactionChargeType(String transactionChargeType) => 
+      this._transactionChargeType = transactionChargeType; 
 
-    //implement setters and getters the dart way
-    
+  String get getTransactionCharge => this._transactionCharge; 
+  set setTransactionCharge(String transactionCharge) => 
+      this._transactionCharge = transactionCharge; 
 }
