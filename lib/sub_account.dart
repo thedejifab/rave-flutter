18ba77e3c5d130c2ce105a312e2cc3f7193bf613
Fabusuyi012 <thedejifab@outlook.com>
2018-12-23 00:35:39 +0100

    Added constructors for implementation of SubAccount class. Constructors use placeholder name in the mean time since Dart only allows 1 unnamed constructor at a time. We'll figure the rest out later.. hahaha

--- lib/sub_account.dart
+++ lib/sub_account.dart
@@ -2,10 +2,17 @@ class SubAccount {
   static final String FLAT = "flat"; 
   static final String PERCENTAGE = "percentage"; 
 
+  // Expose to allow JSON serialization
   String _id; 
   String _transactionSplitRatio; 
   String _transactionChargeType; 
   String _transactionCharge; 
+
+  SubAccount();
+
+  SubAccount.halfFledged(this._id, this._transactionSplitRatio);
+
+  SubAccount.fullFledged(this._id, this._transactionSplitRatio, this._transactionChargeType, this._transactionCharge);
 
   String get getID => this._id; 
   set setID(String id) => 
