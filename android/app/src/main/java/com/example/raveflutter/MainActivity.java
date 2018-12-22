7d5e0ea144c097173bccf283ea77acf543238add
Fabusuyi012 <thedejifab@outlook.com>
2018-12-22 21:40:56 +0100

    added rave-android dependencies and listed major classes and methods to be created

--- android/app/src/main/java/com/example/raveflutter/MainActivity.java
+++ android/app/src/main/java/com/example/raveflutter/MainActivity.java
@@ -1,13 +1,40 @@
-package com.example.raveflutter;
+package com.example.raveflutter; 
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import android.os.Bundle; 
+import io.flutter.app.FlutterActivity; 
+import io.flutter.plugins.GeneratedPluginRegistrant; 
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    super.onCreate(savedInstanceState); 
+    GeneratedPluginRegistrant.registerWith(this); 
   }
+
+  // payments collection methods   
+  // setAmount(amount); 
+  // setCountry(country);  - accepts type country, create country class
+  // setCurrency(currency);  - accepts type currency, create currency class
+  // setfName(fName); 
+  // setlName(lName); 
+  // setEmail(email); 
+  // setNarration(narration); 
+  // setPublicKey(publicKey); 
+  // setEncryptionKey(encryptionKey); 
+  // setTxRef(txRef);  - acccepts type Object
+  // acceptAccountPayments(boolean); 
+  // acceptCardPayments(boolean); 
+  // acceptMpesaPayments(boolean); 
+  // acceptGHMobileMoneyPayments(boolean); 
+  // acceptAchPayments(boolean); 
+  // onStagingEnv(boolean); 
+  // setMeta(List < Meta > ); 
+  // setSubAccounts(List < SubAccount > );  - create SubAccount class
+  // setIsPreAuth(boolean); 
+  // withTheme(styleId); 
+  // setPaymentPlan(payment_plan); 
+  // initialize(); 
+  
+  
+    
 }
